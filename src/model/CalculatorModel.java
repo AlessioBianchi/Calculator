dfbc03cd98ab795a592dba12f7e9377aaede5388
AlessioBianchi <32877086+AlessioBianchi@users.noreply.github.com>
2025-03-05 09:01:41 +0100

    Update CalculatorModel.java
    manage decimal

--- src/model/CalculatorModel.java
+++ src/model/CalculatorModel.java
@@ -8,45 +8,52 @@ import observer.Subject;
 public class CalculatorModel implements Subject {
     private ArrayList<Observer> observers;
 
-    private double number;
+    private String number;
     private double previousNumber;
     private String currentTypeOfOperation;
+    private int counter;
 
     public CalculatorModel () {
         observers = new ArrayList<>();
-        number = 0;
+        number = "0";
         previousNumber = 0;
         currentTypeOfOperation = "";
+        counter = 0;
     }
 
-    public void addDigit (float digit) {
-        setNumber((getNumber() * 10) + digit);
+    public void addDigit (String digit) {
+    	if (number.equals("0")) {
+    		setNumber(digit);
+    	} else {
+    		setNumber(getNumber() + digit);
+    	}
     }
 
     public Double makeOperation () {
         Double result = 0.0;
+        Double n = Double.valueOf(this.getNumber());
         switch (this.currentTypeOfOperation) {
             case "+":
-                result = this.getPreviousNumber() + this.getNumber();
+                result = this.getPreviousNumber() + n;
                 break;
             case "-":
-                result = this.getPreviousNumber() - this.getNumber();
+                result = this.getPreviousNumber() - n;
                 break;
             case "x":
-                result = this.getPreviousNumber() * this.getNumber();
+                result = this.getPreviousNumber() * n;
                 break;
             case "/":
-                result = this.getPreviousNumber() / this.getNumber();
+                result = this.getPreviousNumber() / n;
                 break;
         }
         return result;
     }
 
-    public double getNumber () {
+    public String getNumber () {
         return number;
     }
 
-    public void setNumber (double number) {
+    public void setNumber (String number) {
         this.number = number;
         notifyObservers();
     }
@@ -63,8 +70,16 @@ public class CalculatorModel implements Subject {
         return previousNumber;
     }
 
-    public void setPreviousNumber(double previousNumber) {
-        this.previousNumber = previousNumber;
+    public void setPreviousNumber(String previousNumber) {
+        this.previousNumber = Double.valueOf(previousNumber);
+    }
+    
+    public int getCounter() {
+    	return counter;
+    }
+    
+    public void incCounter() {
+    	this.counter++;
     }
 
     @Override
